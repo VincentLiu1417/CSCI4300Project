285b980db4546f48dd8b1d9dc7d8918dd22b9a49
Dev Patel <dev7712@gmail.com>
2023-12-03 16:26:00 -0500

    shopping cart update

--- final-project/src/components/CartList.js
+++ final-project/src/components/CartList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CartList.css'; // You can create a CSS file for styling
 
 const Cart = ({ name, price, image }) => (
@@ -10,27 +10,38 @@ const Cart = ({ name, price, image }) => (
 );
 
 
-const Cart1 = ({ name, price, img }) => (
+const Cart1 = ({ name, price, img, updateQuantity }) => {
+  const [quantity, setQuantity] = useState(0); 
+  const [totalPrice, setTotalPrice] = useState(0); 
+
+  const handleQuantityChange = (event) => {
+    const newQuantity = parseInt(event.target.value, 10);
+    setQuantity(newQuantity);
+    updateQuantity(name, newQuantity);
+  };
+  return (
     <div class='cartcard'>
         <img src={img} alt={name} class="produce"/>
         <h2 class='cartproduceHdr'>{name}</h2>
-        <p class='cartprice'>{price}</p>
+        <p class='cartprice'>${price}</p>
         <p class='quantity'>Quantity:</p>
         <form class='quantityform' action="/action_page.php">
             <label for="quantity">+ or - : </label>
-            <input type="number" id="quantity" name="quantity" min="1" max="9"></input>
+            <input type="number" id="quantity" name="quantity" min="0" max="9" value={quantity} onChange={handleQuantityChange}></input>
         </form>
-        <div class='quantitytotal'><p class='totalprice'>{price}</p></div>
+        <div class='quantitytotal'><p class='totalprice'>${quantity * price}</p></div>
         <div class='savetotal'><p class='saveprice'>Saved: $0.00</p></div>
       </div>
+      
   );
+};
 
-const CartList = ({ items }) => (
+const CartList = ({ items, handleQuantityChange }) => (
   <div class='cartlistDisplay'>
     {items.slice(0, 3).map((item, index) => (
-      <Cart1 key={index} {...item} />
+      <Cart1 key={index} {...item} updateQuantity={handleQuantityChange}/>
     ))}
   </div>
 );
-
+    
 export { Cart1, CartList };
--- final-project/src/components/ShoppingCart.js
+++ final-project/src/components/ShoppingCart.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; 
 import './ShoppingCart.css';
 import shoppinPic from "../images/shoppinpic.jpg";
 import veggiesPic from "../images/veggies.jpg";
@@ -10,28 +11,50 @@ import { Cart1, CartList } from './CartList';
 
 function Cart({arr}) {
 
-  const DummyArray = [
+  const [cartItems, setCartItems] = useState([
     {
       name:"Orange Carrots  - 1lbs",
       img: "https://pngimg.com/uploads/carrot/carrot_PNG4985.png",
-      price:"$2.99"
+      price:"2.99",
+      quantity:0,
     },
     {
       name:"One Dozen Brown Eggs  - 12ct",
       img:"https://th.bing.com/th/id/R.2afed416987f2f8f4807362a2f6c64c8?rik=WVA0x30udG%2fIaA&pid=ImgRaw&r=0",
-      price:"$5.99"
+      price:"5.99",
+      quantity:0,
     },
     {
       name:"Sirloin Cut - 1lb",
       img:'https://th.bing.com/th/id/OIP.9hJSAsh0ucz01swtcwTXhAHaDc?pid=ImgDet&rs=1',
-      price:"$10.99"
+      price:"10.99",
+      quantity:0,
     },
     {
       name:"Oranges - 1 lb",
       img:"https://th.bing.com/th/id/R.3f729df06a50706123e89c7b34126d59?rik=IaByHOxT1hEsBQ&pid=ImgRaw&r=0",
-      price:"$7.99"
+      price:"7.99",
+      quantity:0,
     },
-  ];
+  ]);  
+
+  const handleQuantityChange = (itemName, newQuantity) => {
+    const updatedItems = cartItems.map(item => {
+      if (item.name === itemName) {
+        return { ...item, quantity: newQuantity};
+      }
+      return item;
+    });
+    setCartItems(updatedItems);
+  };
+
+  const calculateTotalPrice = () => {
+    let total = 0;
+    cartItems.forEach(item => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  };
 
 
 
@@ -47,16 +70,16 @@ function Cart({arr}) {
       <div class="horizontal-line"></div>
 
       <div class='continueShop'>
-        <button class="continueShopbutton">Continue Shopping</button>
+        <Link to="/" class="continueShopbutton">Continue Shopping</Link>
       </div>
 
       <div class='cardShowcase'>
-        <CartList items={DummyArray} />
+        <CartList items={cartItems} handleQuantityChange={handleQuantityChange} />
       </div>
 
       <div class='subtotal'>
         <p class='checkoutprice'>Total Price: </p>
-        <p class='checkoutprice'> $19.97 </p>
+        <p class='checkoutprice'> ${calculateTotalPrice()} </p>
         <div class='checkoutbtn'><button class='checkout'>Checkout</button></div>
       </div>
 
